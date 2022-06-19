c008714444fd34ca594864843a3e9cc575898c9f
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-06-19 18:47:16 +0530

    local storage web api

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const arr =[1,2,3]
-const obj = {
-    name:"C class",
-    brand:"Mercedes",
-    model:2022,
-    log:()=>(console.log('This is mercedes'))
-}
-console.log(console)
+// web API's
 
-console.date = ()=>{
-    console.log(new Date)
-}
-
-console.log(window.innerHeight)
-console.log(window.innerWidth)
-
-// const name = window.prompt("what is your name?")
-
-window.open("https://youtube.com","_blank")
-console.log(window.location.href)
-
-
-
-
+//localStorage.setItem('model',"C class")
+localStorage.removeItem('brand')
