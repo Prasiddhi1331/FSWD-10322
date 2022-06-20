6f1235a64b7a9955dca6d3702a342999ae5d548f
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-06-21 00:33:14 +0530

    array methods

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const a = ()=>{}
 
 const isPresent = arr.find(callback)
 console.log(isPresent)
+
+//const newArr = arr.filter((value,index) => valye%2===0?true:false)
+//console.log(newArr)
+
+//const num = 10;
+
+//const result = num%2===0 ? "even": "odd"
+//console.log(result)
