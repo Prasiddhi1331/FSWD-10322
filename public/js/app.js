796d905b36eb67c20885bd24a86b8164b2ff139c
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-07-05 01:01:00 +0530

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,15 @@
 //could also use nth child selector
 
+let user_score = 0;
+let computer_score = 0;
+
+const user_board = document.querySelector('div.scoreboard .user')
+const computer_board = document.querySelector('div.scoreboard .comp')
+
+const message_box = document.querySelector("div.message")
+
+console.log(user_score,computer_score)
+
 const_rock = document.querySelector("#rock")
 const_paper = document.querySelector("#paper")
 const_scissors = document.querySelector("#scissors")
@@ -14,6 +24,22 @@ const generateComputerResponse = ()=>{
   return choices[index]
 }
 
+const result = (winner, userResponse, computerResponse)=>{
+  switch(winner){
+    case "user":
+      message_box.innerHTML= 'User Wins'
+      user_board.innerHTML=++user_score
+      /*document.querySelector('#${userResponse}').style="border-color:green"*/
+      break
+    case "computer":
+      message_box.innerHTML='Computer Wins'
+      computer_board.innerHTML=++computer_score
+      break
+    default:
+      window.alert("something went wrong") 
+  }
+}
+
 const playGame = (userResponse)=>{
   const computerResponse = generateComputerResponse()
   console.log(userResponse===computerResponse)
@@ -22,30 +48,30 @@ const playGame = (userResponse)=>{
     case "rock":
       switch(computerResponse){
         case "paper":
-          console.log('computer wins')
+          return result("computer",userResponse,computerResponse)
           break
         case "scissors":
-          console.log('user wins')
+          return result("user",userResponse,computerResponse)
           break
       }
       break;
       case "paper":
         switch(computerResponse){
           case "rock":
-            console.log('user wins')
+            return result("user",userResponse,computerResponse)
             break
           case "scissors":
-            console.log('computer wins')
+            return result("computer",userResponse,computerResponse)
             break
         }
         break;
       case "scissors":
         switch(computerResponse){
           case "rock":
-            console.log('computer wins')
+            return result("computer",userResponse,computerResponse)
             break
           case "paper":
-            console.log('user wins')
+            return result("user",userResponse,computerResponse)
             break
         }
         break;
