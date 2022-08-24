0d70b91ca69534f8feb0478f276822b6b2aa19be
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-08-24 22:03:45 +0530

    change

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
 const endpoint = "https://jsonplaceholder.typicode.com/todos"
 
-const response = fetch(endpoint);
+const button =document.querySelector('button')
+const list =document.querySelector('ol')
 
-response.then ( (data_stream)=>{
-    const data = data_stream.json()
-
-    data.then(finalData=>{
-        console.log(finalData)
+const fetchData = ()=>{
+    fetch(endpoint)
+    .then(stream=>stream.json())
+    .then(todos=>{
+        todos.map(todo=>{
+            const listelement = document.createElement('li')
+            listelement.innerHTML=todo.title
+            const isCompleted =todo.completed
+            if(isCompleted){
+                listelement.style="color:lime"
+            }else{
+                listelement.style="color:crimson"
+            }
+            console.log(listelement)
+            list.appendChild(listelement)
+        })
     })
-})
+}
 
+button.addEventListener('click',fetchData)
