511d402999e9bb238bcfc5248e89cd384344d426
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-03 18:52:36 +0530

    os module

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-//console.log("hello world")
+const os = require('os')
 
-console.log(window)
+console.log(os.uptime())
+console.log(os.homedir())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.userInfo())
+console.log(os.freemem())
+console.log(os.hostname())
+console.log(os.version())
+console.log(os.release())
