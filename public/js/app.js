34be4943c8be8303d125d2508a386390bf22ed08
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-03 14:47:33 +0530

    recipe api

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,19 @@
-const endpoint = "https://jsonplaceholder.typicode.com/todos"
+const app_id ="b5cd15c0";
+const app_key ="db0150d7edcb38a5380fdeb3d6946de0";
+const button = document.querySelector('button')
+const input = document.querySelector('input')
 
-const getData = async ()=>{
-    try {
-        const response = await fetch(endpoint)
-        const jsonData = await response.json()
-        console.log(jsonData)
-    } catch(error) {
-        console.log(error)
-    }
+const getRecipe = async (query) => {
+    const endpoint = 'https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=b5cd15c0&app_key=db0150d7edcb38a5380fdeb3d6946de0';
+    const response= await fetch(endpoint)
+    const data = await response.json()
+    console.log(data)
+};
+
+
+const searchRecipe = ()=>{
+    const query = input.value
+    getRecipe(query)
 }
 
-getData()
+button.addEventListener('click',searchRecipe)
