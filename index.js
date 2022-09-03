3bdfa5fa6b9564d661763f502d70f6f545b33e29
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-04 00:52:31 +0530

    server with http module

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req,res)=>{
-    res.write("<h1>PING from server</h1>")
-    res.end();
-})
+    if(req.url==='/'){
+        const data = fs.readFileSync(path.join(__dirname,'/public/home.html'))
+        res.write(data)
+    }else if(req.url==='/about'){
+        const data = fs.readFileSync(path.join(__dirname,'/public/about.html'))
+        res.write(data)
+    }
+        res.end()
+    })  
 
-server.listen(4000)
+
+server.listen(8080)
