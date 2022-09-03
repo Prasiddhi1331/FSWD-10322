be87a8cd2f53cc04006e21fcb8f1503390521b2d
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-04 02:13:32 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
 
 const server = http.createServer((req,res)=>{
+    res.writeHead(403,{
+        "custom":"custom value",
+        "date": new Date().getDate(),
+        version:os.version()
+    })
+    
     if(req.url==='/'){
         const data = fs.readFileSync(path.join(__dirname,'/public/home.html'))
         res.write(data)
     }else if(req.url==='/about'){
         const data = fs.readFileSync(path.join(__dirname,'/public/about.html'))
         res.write(data)
+    }else if(req.url==='/photo'){
+        const data = fs.readFileSync(path.join(__dirname,'/public/image.jpg'))
+        res.write(data)
     }
+    res.writeHead(200,{
+        'custom':"something"
+    })
         res.end()
     })  
 
