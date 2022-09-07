51af2418ef5d711d186e221c06b82bf02b4d1b89
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-08 01:19:28 +0530

    expree server

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express")
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('response')
+    res.send('home page')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('about page')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('contact page')
 })
 
 app.listen(8080,()=>{
