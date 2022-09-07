2b6f257948c3587d0ab8eb7ca938273a233f40ed
Prasiddhi1331 <prasiddhisingh1331@gmail.com>
2022-09-08 00:55:31 +0530

    express server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('hi')
-console.log('this is prasiddhi')
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('response')
+})
+
+app.listen(8080,()=>{
+    console.log('server started at port: ${8080}')
+})
